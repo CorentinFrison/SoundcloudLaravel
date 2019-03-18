fa2a3ea98782d2a28a6985c696315813905988e4
adrien.dufour <ad.dufour16@gmail.com>
2019-03-18 16:28:20 +0100

    Css

--- sound2019/resources/views/_chansons.blade.php
+++ sound2019/resources/views/_chansons.blade.php
@@ -1,4 +1,8 @@
-@foreach($chansons as $c)
+
+<div id="grid2">
+       @foreach($chansons as $c)
+              <div class="main_chanson">
        <a href="#" class="chanson" data-file="{{$c->fichier}}" data-pjax>{{$c->nom}}</a> écrite par <a href="/utilisateur/{{$c->utilisateur->id}}" data-pjax>{{$c->utilisateur->name}}</a><br>
-   
+              </div>
    @endforeach
+</div>
--- sound2019/resources/views/utilisateur.blade.php
+++ sound2019/resources/views/utilisateur.blade.php
@@ -3,26 +3,33 @@
 @section('content')
 
     @guest
-    <h3>Page de : {{$utilisateur->name}}</h3>
-        Il suit {{$utilisateur->jeLesSuit->count()}} personne(s)<br/>
-        Suivi par {{$utilisateur->ilsMeSuivent->count()}} personne(s)
-    @endguest
+    <h3>{{$utilisateur->name}}</h3>
+    <div class="follow">
+        Follow {{$utilisateur->jeLesSuit->count()}} personne(s)<br/>
+        Follower {{$utilisateur->ilsMeSuivent->count()}} personne(s)
+    </div>
+        @endguest
 
     @auth
         @if(Auth::id() != $utilisateur->id)
-        <h3>Page de : {{$utilisateur->name}}</h3>
-        Il suit {{$utilisateur->jeLesSuit->count()}} personne(s)<br/>
-        Suivi par {{$utilisateur->ilsMeSuivent->count()}} personne(s)<br/>
+        <h3>{{$utilisateur->name}}</h3>
+        <div class="follow">
+        Follow {{$utilisateur->jeLesSuit->count()}} personne(s)<br/>
+        Follower {{$utilisateur->ilsMeSuivent->count()}} personne(s)
+        </div>
+            <br/>
             @if($utilisateur->ilsMeSuivent->contains(Auth::id()))
-                <a href="/suivre/{{$utilisateur->id}}" data-pjax >Arreter de suivre</a>
+                <a href="/suivre/{{$utilisateur->id}}" data-pjax ><img class="img" src="{{asset('/public/img/icon_croix.png')}}" width="100px"/></a>
             @else
-                <a href="/suivre/{{$utilisateur->id}}" data-pjax >Suivre</a>
+                <a href="/suivre/{{$utilisateur->id}}" data-pjax ><img class="img" src="{{asset('/public/img/icon_heart.png')}}" width="100px"/></a>
             @endif
         @else
-        <h3>Ma page</h3>
-        Je suis {{$utilisateur->jeLesSuit->count()}} personne(s)<br/>
-        Je suis suivi par {{$utilisateur->ilsMeSuivent->count()}} personne(s)<br/>
-        @endif
+        <h3>Mon profil</h3>
+        <div class="follow">
+        Follow {{$utilisateur->jeLesSuit->count()}} personne(s)<br/>
+        Follower {{$utilisateur->ilsMeSuivent->count()}} personne(s)<br/>
+        </div>
+            @endif
     @endauth
 
 <br>
